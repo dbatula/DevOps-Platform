c83a096fd6804c12c5831a0edbd9abfcf37dcc8c
Daniil Batula <batula.daniil.official@gmail.com>
2026-06-09 17:05:38 +0200

    added DOckerfile and .dockerignore to all services

--- services/auth-service/auth.py
+++ services/auth-service/auth.py
@@ -1,6 +1,6 @@
-import jwt
+import jwt # type: ignore
 import datetime
-from passlib.context import CryptContext
+from passlib.context import CryptContext # type: ignore
 
 SECRET_KEY = "secret123"
 
--- services/auth-service/main.py
+++ services/auth-service/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI # type: ignore
+from pydantic import BaseModel # type: ignore
 from auth import hash_password, verify_password, create_token
 
 app = FastAPI()
