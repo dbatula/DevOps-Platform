766c480d4634f07b69adf4742d7f67599a5c3133
Daniil Batula <batula.daniil.official@gmail.com>
2026-06-17 12:53:05 +0200

    add # type: ignore to python libraries

--- services/user-service/database.py
+++ services/user-service/database.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine # type: ignore
+from sqlalchemy.orm import sessionmaker # type: ignore
 
 DATABASE_URL = "postgresql://devops:devops@postgres:5432/microservices"
 
